0a23017943cec86e18cbd4c0e956499d3c665603
Rebin <muhammedrebin1794@gmail.com>
2024-10-05 12:28:16 +0530

    BFS and DFS traversal

---
+++ Data Structures/07.Trees & Binary Trees/BFS_and_DFS.py
@@ -0,0 +1,79 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def bread_first(self):
+        if not self.root:
+            return []
+            
+        array = []
+        queue = [self.root]
+        
+        while queue:
+            node = queue.pop(0)
+            array.append(node.value)
+            
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            
+        return array
+
+    def dfs_in_order(self):
+        if not self.root:
+            return []
+            
+        array = []
+        current = self.root
+        
+        def trav(node):
+            if node.left:
+                trav(node.left)
+            array.append(node.value)
+            if node.right:
+                trav(node.right)
+                
+        trav(current)
+        return array
+
+    def dfs_pre_order(self):
+        if not self.root:
+            return []
+            
+        array = []
+        current = self.root
+        
+        def trav(node):
+            array.append(node.value)
+            if node.left:
+                trav(node.left)
+            if node.right:
+                trav(node.right)
+                
+        trav(current)
+        return array
+
+    def dfs_post_order(self):
+        if not self.root:
+            return []
+            
+        array = []
+        current = self.root
+        
+        def trav(node):
+            if node.left:
+                trav(node.left)
+            if node.right:
+                trav(node.right)
+            array.append(node.value)
+                
+        trav(current)
+        return array
